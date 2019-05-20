1d7d634f22f0b110d6561c2743b92b3047bedea5
ingecraft <ingecraft@theoneswhodid.com>
2019-05-20 21:36:38 +0300

    Added db initialization and configuration

--- tests/test_appplication_setup.py
+++ tests/test_appplication_setup.py
@@ -16,3 +16,6 @@ class ApplicationSetupTestCase(unittest.TestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
+
+    def test_sqlalchemy(self):
+        pass
