49c59facb61dca77c56d6e273413fc4fbb5741f1
ingecraft <ingecraft@theoneswhodid.com>
2019-05-31 03:20:24 +0300

    Added operator and phone call models

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ class Lead(db.Model):
     surname = db.Column(db.String(64))
     phone_number = db.Column(db.String(10))
     email = db.Column(db.String(64), unique=True, index=True)
+    phone_call_id = db.Column(db.Integer, db.ForeignKey('phone_calls.id'))
+    phone_call = db.relationship('PhoneCall')
 
     def __repr__(self):
         return '<Lead %r>' % self.email
@@ -19,12 +21,20 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Operator(db.Model):
+    __tablename__ = 'operators'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    phone_call = db.relationship("PhoneCall", uselist=False, back_populates="operators")
 
     def __repr__(self):
         return '<User %r>' % self.username
-    
+
+class PhoneCall(db.Model):
+    __tablename__ = 'phone_calls'
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime, index=True)
+    operator_id = db.Column(db.Integer, db.ForeignKey('operator.id'))
+    operator = db.relationship("Operator", back_populates="phone_calls") 
+
 
